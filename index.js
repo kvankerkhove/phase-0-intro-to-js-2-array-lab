dba69281055d850f99b0a327925f3b13816e90bb
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-04-05 13:34:05 -0400

    commit tree 6716f9cfb67617eb5b4e8931c6f7e77de6ffbcbb

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const newAppendCat = [...cats];
+    newAppendCat.push("Broom");
+    return newAppendCat;
+}
+
+function prependCat(name) {
+    const newPrependCat = [...cats];
+    newPrependCat.unshift("Arnold");
+    return newPrependCat;
+}
+
+function removeLastCat(name) {
+    const newRemoveLastCat = [...cats];
+    newRemoveLastCat.pop();
+    return newRemoveLastCat;
+}
+
+function removeFirstCat(name) {
+    const newRemoveFirstCat = [...cats];
+    newRemoveFirstCat.shift();
+    return newRemoveFirstCat;
+}
